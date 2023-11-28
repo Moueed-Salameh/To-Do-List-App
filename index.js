921c41fb62b5d3496b1ba26913c26e1ecc0d2e49
Moueed-Salameh <salamehmoueed@gmail.com>
2023-11-28 14:11:47 +0300

    add task delete functionality

--- index.js
+++ index.js
@@ -8,18 +8,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let posts = [];
+let lastId = 0;
 
 app.get("/", (req, res) => {
   res.render("index.ejs", {data: posts});
 });
 
 app.post("/submit", (req, res) => {
-    posts.push({
-        tskTitle: req.body["taskTitle"],
-        tskDesc: req.body["taskDesc"],
-    });
-    res.redirect("/");
+  posts.push({
+      tskId: lastId,
+      tskTitle: req.body["taskTitle"],
+      tskDesc: req.body["taskDesc"],
+  });
+  lastId += 1;
+  res.redirect("/");
 });
+
+app.post("/checkoff", (req,res) => {
+  Object.keys(req.body).forEach((key) => {
+    const deleteId = parseInt(key.slice(5));
+    const deleteIndex = posts.findIndex((post) => post.tskId === deleteId);
+    posts.splice(deleteIndex, 1);
+  });
+  res.redirect("/");
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
